d7ac8c8b2f7973e5ecaf363642e3e41c3f045bbf
Kent Fredric <kentfredric@gmail.com>
2011-04-14 07:26:07 +1200

    some tests and stub files to make sure it works right

--- lib/Test/CPAN/Changes/ReallyStrict.pm
+++ lib/Test/CPAN/Changes/ReallyStrict.pm
@@ -30,7 +30,7 @@ happy, you'll have to update your whole changes file if upstream changes how the
 use CPAN::Changes;
 use Test::Builder;
 
-my $test       = Test::Builder->new();
+my $Test       = Test::Builder->new();
 my $version_re = '^[._\-[:alnum:]]+$';    # "Looks like" a version
 
 sub import {
@@ -51,14 +51,14 @@ sub _config {
   if ( not defined $config ) {
     $config = {};
   }
-  if ( ( not exists $config->{remove_empty_sections} ) or ( not defined $config->{remove_empty_sections} ) ) {
-    $config->{remove_empty_sections} = 0;
-  }
-  else {
 
-    # force boolean context.
-    $config->{remove_empty_sections} = !!$config->{remove_empty_sections};
+  # force boolean context.
+  $config->{delete_empty_groups} = !!$config->{delete_empty_groups};
+  $config->{keep_comparing}      = !!$config->{keep_comparing};
+  if( defined $config->{next_style} and $config->{next_style} = 'dzil' ){
+    $config->{next_token} = qr/{{\$NEXT}}/,
   }
+
   return $config;
 }
 
@@ -66,105 +66,176 @@ sub changes_file_ok {
   my ( $file, $config ) = @_;
   $file ||= 'Changes';
   my $real_config = _config($config);
+  $config->{filename} = $file;
+  _real_changes_file_ok( $Test, $real_config );
+}
+sub _real_changes_file_ok {
+  my ( $tester, $state ) = @_;
+  return unless _test_load( $tester, $state );
+  return unless _test_has_releases( $tester, $state );
+  return unless _test_releases( $tester, $state );
+  return unless _stash_original_content( $tester, $state );
+  return unless _prune( $tester, $state );
+  return unless _stash_serialized_content( $tester, $state );
+  return unless _test_lines( $tester, $state );
+  return 1;
+}
+sub _test_load {
+  my ( $tester, $state ) = @_;
 
-  my $changes = eval { CPAN::Changes->load($file) };
+  my @extra;
+  if( defined $state->{next_token} ){
+    @extra = ( next_token => $state->{next_token} );
+  }
+  my ( $error, $changes );
+  {
+    local $@;
 
-  if ($@) {
-    $Test->ok( 0, "Unable to parse $file" );
-    $Test->diag("  ERR: $@");
+    eval { $changes = CPAN::Changes->load( $state->{filename}, @extra );  };
+    $error = $@;
+  }
+
+  if ( not $error ) {
+    $tester->ok( 1, $state->{filename} . ' is loadable' );
+    $state->{changes} = $changes;
+    return 1;
+  }
+
+  $tester->ok( 0, 'Unable to parse ' . $state->{filename} );
+  $tester->diag($error);
+  return;
+}
+
+sub _test_has_releases {
+  my ( $tester, $state ) = @_;
+  # dump [ '_test_has_releases' , $tester, $state ];
+  my (@releases) = $state->{changes}->releases;
+  if (@releases) {
+    $tester->ok( 1, $state->{filename} . ' contains at least one release' );
+    return 1;
+  }
+  $tester->ok( 0, $state->{filename} . ' does not contain any release' );
+  return;
+}
+
+sub _test_release_date {
+  my ( $tester, $state ) = @_;
+  return 1 if not defined $state->{release}->date and defined $state->{next_token};
+  return 1 if $state->{release}->date =~ m[^${CPAN::Changes::W3CDTF_REGEX}\s*$];
+  $tester->ok( 0, $state->{file} . ' contains an invalid release date' );
+  $tester->diag( '  ERR: ' . $state->{release}->date );
+  return;
+}
+
+sub _test_release_version {
+  my ( $tester, $state ) = @_;
+  return 1 if not defined $state->{release}->version and defined $state->{next_token};
+  return 1 if defined $state->{next_token} and $state->{release}->version =~ $state->{next_token};
+  return 1 if ( $state->{release}->version =~ m[$version_re] );
+  $tester->ok( 0, $state->{filename} . ' contains an invalid release version number' );
+  $tester->diag( '  ERR: ' . $state->{release}->version );
+  return;
+}
+
+sub _test_releases {
+  my ( $tester, $state ) = @_;
+  for ( $state->{changes}->releases ) {
+    local $state->{release} = $_;
+    return unless _test_release_date( $tester, $state );
+    return unless _test_release_version( $tester, $state );
+  }
+  $tester->ok( 1, $state->{filename} . ' contains valid release dates' );
+  $tester->ok( 1, $state->{filename} . ' contains valid version numbers' );
+  return 1;
+}
+
+sub _stash_original_content {
+  my ( $tester, $state ) = @_;
+
+  my $fh;
+  if ( not open $fh, '<', $state->{filename} ) {
+    $tester->ok( 0, $state->{filename} . ' failed to open' );
+    $tester->diag( 'Error ' . $! );
     return;
   }
+  local $/ = undef;
+  my $str = <$fh>;
 
-  $Test->ok( 1, "$file is loadable" );
+  my @lines = split /\n/, $str;
 
-  my @releases = $changes->releases;
+  $state->{original_lines} = \@lines;
+  return 1;
+}
 
-  if ( !@releases ) {
-    $Test->ok( 0, "$file does not contain any releases" );
-    return;
-  }
+sub _prune {
+  my ( $tester, $state ) = @_;
+  return 1 unless $state->{delete_empty_groups};
 
-  $Test->ok( 1, "$file contains at least one release" );
-
-  for (@releases) {
-    if ( $_->date !~ m{^${CPAN::Changes::W3CDTF_REGEX}\s*$} ) {
-      $Test->ok( 0, "$file contains an invalid release date" );
-      $Test->diag( '  ERR: ' . $_->date );
-      return;
-    }
-    if ( $_->version !~ m{$version_re} ) {
-      $Test->ok( 0, "$file contains an invalid version number" );
-      $Test->diag( '  ERR: ' . $_->version );
-      return;
-    }
-  }
-
-  $Test->ok( 1, "$file contains valid release dates" );
-  $Test->ok( 1, "$file contains valid version numbers" );
+  my ( $rval, $error );
 
   {
-
-    # linewise test.
-    my $fh;
-
-    if ( not open $fh, '<', $file ) {
-      $Test->ok( 0, "$file failed to open" );
-      $Test->diag("file: $file error:$!");
-      return;
-    }
-
-    my (@orig) = <$fh>;
-    chomp for @orig;
-
-    if ( $real_config->{delete_empty_groups} ) {
-      if ( eval { CPAN::Changes->VERSION('0.14'); 1 } ) {
-        $changes->delete_empty_groups;
-      }
-      else {
-        $Test->note("delete_empty_groups not available in CPAN::Changes $CPAN::Changes::VERSION");
-      }
-    }
-    my $serialized = $changes->serialize;
-
-    my (@out) = split /\n/, $serialized;
-
-    my $success = 1;
-
-    for ( 0 .. $#out ) {
-      if ( $orig[$_] ne $out[$_] ) {
-        if ($success) {
-          $Test->ok( 0, "Lines differ from standard on line $_ in $file" );
-          $Test->diag( 'Expected: >' . $out[$_] . '<<EOL' );
-          $Test->diag( 'Got:      >' . $orig[$_] . '<<EOL' );
-          $success = 0;
-        }
-        else {
-          $Test->note( $_ . '<> Expected: >' . $out[$_] . '<<EOL' );
-          $Test->note( $_ . '<> Got:      >' . $orig[$_] . '<<EOL' );
-        }
-      }
-    }
-
-    if ( scalar @orig != scalar @out ) {
-      if ($success) {
-        $Test->ok( 0, "Number of lines mismatched" );
-        $Test->diag( "Source" . scalar @orig );
-        $Test->diag( "Expected" . scalar @out );
-        $success = 0;
-      }
-      else {
-        $Test->note("Number of lines mismatched");
-        $Test->note( "Source" . scalar @orig );
-        $Test->note( "Expected" . scalar @out );
-
-      }
-    }
-
-    if ($success) {
-      $Test->ok( 1, "Input and output are identical" );
-    }
+    local $@;
+    $rval = eval { CPAN::Changes->VERSION('0.14'); 1 };
+    $error = $@;
   }
 
+  if ( $error or not $rval ) {
+    $tester->ok( 0, 'delete_empty_groups not supported on CPAN::Changes prior to 0.14' );
+    $tester->diag("CPAN::Changes Version: $CPAN::Changes::VERSION");
+    return;
+  }
+
+  $state->{changes}->delete_empty_groups();
+
+  return 1;
 }
+
+sub _stash_serialized_content {
+  my ( $tester, $state ) = @_;
+
+  my $string = $state->{changes}->serialize;
+
+  my (@out) = split /\n/, $string;
+
+  $state->{generated_lines} = \@out;
+
+  return 1;
+
+}
+
+sub _test_line {
+  my ( $tester, $state ) = @_;
+  return 1 if defined $state->{line}->{original} and defined $state->{line}->{generated} and $state->{line}->{original} eq $state->{line}->{generated};
+  if ( not $state->{had_first_line_failure} ) {
+    $tester->ok( 0, 'Lines differ at line ' . $state->{line}->{no} );
+    $state->{had_first_line_failure} = 1;
+  }
+  $tester->diag( '[' . $state->{line}->{no} . '] Expected: ' . ( defined $state->{line}->{generated} ? '>' . $state->{line}->{generated}  . '<' : 'not defined' ) );
+  $tester->diag( '[' . $state->{line}->{no} . '] Got     : ' . ( defined $state->{line}->{original} ? '>' . $state->{line}->{original}  . '<' : 'not defined' ) );
+  return;
+}
+
+sub _test_lines {
+  my ( $tester, $state ) = @_;
+
+  my (@original)  = @{ $state->{original_lines} };
+  my (@generated) = @{ $state->{generated_lines} };
+
+  for ( 0 .. $#original ) {
+    local $state->{line} = { original => $original[$_], generated => $generated[$_], no => $_ };
+    return if not _test_line( $tester, $state ) and not $state->{keep_comparing};
+  }
+  if( $state->{keep_comparing} and not $state->{had_first_line_failure} ){
+    $tester->ok( 1, 'All lines in original match generated' );
+    return 1;
+  }
+
+  if( not $state->{keep_comparing} ){
+    return 1;
+  }
+  return;
+}
+
+
 
 1;
---
+++ t/02_basic_mock_inprogress.t
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+
+use Test::More 0.96;
+use Test::MockObject;
+use FindBin;
+
+my $mock = Test::MockObject->new();
+
+my @events;
+
+$mock->mock(
+  'ok' => sub {
+    my ( $self, @args ) = @_;
+    push @events, [ 'ok', @args ];
+  }
+);
+
+$mock->mock(
+  'diag' => sub {
+    my ( $self, @args ) = @_;
+    push @events, [ 'diag', @args ];
+  }
+);
+
+use Test::CPAN::Changes::ReallyStrict;
+
+ok(
+  !Test::CPAN::Changes::ReallyStrict::_real_changes_file_ok(
+    $mock,
+    {
+      delete_empty_groups => undef,
+      keep_comparing      => undef,
+      filename            => "$FindBin::Bin/../corpus/Changes_02.txt",
+    }
+  ),
+  "Expected bad file is bad ( In progress"
+  )
+  or do {
+  note explain \@events;
+  };
+
+done_testing;
