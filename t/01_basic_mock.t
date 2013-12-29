0a3525fd67520e984557bc8a7116a45acab17b63
Kent Fredric <kentfredric@gmail.com>
2013-12-29 19:26:41 +1300

    Tidy code

--- t/01_basic_mock.t
+++ t/01_basic_mock.t
@@ -9,26 +9,35 @@ my $mock = Test::MockObject->new();
 
 my @events;
 
-$mock->mock('ok' => sub {
-  my ( $self, @args ) = @_ ;
-  push @events, [ 'ok', @args ];
-});
+$mock->mock(
+  'ok' => sub {
+    my ( $self, @args ) = @_;
+    push @events, [ 'ok', @args ];
+  }
+);
 
-$mock->mock('diag' => sub {
-  my ( $self, @args ) = @_;
-  push @events, [ 'diag', @args ];
-});
+$mock->mock(
+  'diag' => sub {
+    my ( $self, @args ) = @_;
+    push @events, [ 'diag', @args ];
+  }
+);
 
 use Test::CPAN::Changes::ReallyStrict;
 
-ok( Test::CPAN::Changes::ReallyStrict::_real_changes_file_ok(
-  $mock, {
-    delete_empty_groups => undef,
-    keep_comparing => undef,
-    filename => "$FindBin::Bin/../corpus/Changes_01.txt",
-  }
-), "Expected good file is good") or do {
+ok(
+  Test::CPAN::Changes::ReallyStrict::_real_changes_file_ok(
+    $mock,
+    {
+      delete_empty_groups => undef,
+      keep_comparing      => undef,
+      filename            => "$FindBin::Bin/../corpus/Changes_01.txt",
+    }
+  ),
+  "Expected good file is good"
+  )
+  or do {
   note explain \@events;
-};
+  };
 
 done_testing;
--- util/zuluize.pl
+++ util/zuluize.pl
@@ -7,26 +7,26 @@ use warnings;
 
 use DateTime::Format::Strptime;
 
-open my $ifh, '<', 'Changes' or die "Can't open Changes, $!";
+open my $ifh, '<', 'Changes'     or die "Can't open Changes, $!";
 open my $ofh, '>', 'Changes.out' or die "Can't open Changes.out for write, $!";
 
-while (defined(my $line = <$ifh>)) {
+while ( defined( my $line = <$ifh> ) ) {
   if ( $line =~ qr{(^[\d.]+\s+)(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\s+([^\s]+)} ) {
-    my $prelude = $1;
-    my $date    = $2;
+    my $prelude  = $1;
+    my $date     = $2;
     my $timezone = $3;
     print "TS: $2 , TZ: $3\n";
     my $strp = DateTime::Format::Strptime->new(
       pattern   => '%Y-%m-%d %H:%M:%S',
       time_zone => $timezone,
     );
-    $line =  join '', ( $prelude, $strp->parse_datetime($date)->set_time_zone('UTC'), "Z\n" );
-  } elsif ( $line =~ /^\s*\d/ ){
+    $line = join '', ( $prelude, $strp->parse_datetime($date)->set_time_zone('UTC'), "Z\n" );
+  }
+  elsif ( $line =~ /^\s*\d/ ) {
     my $lineclone = $line;
     chomp $lineclone;
     print "Datestamp line $lineclone skipped, probably already ok\n";
   }
-  print { $ofh } $line;
+  print {$ofh} $line;
 }
-
 
